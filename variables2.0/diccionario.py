934b75b88c39075e196acbbbd8f1db608716c9a1
Ivan Navas <ivanrng1502@gmail.com>
2023-02-16 22:34:52 -0500

    Prueba de dubida a githup desde visual studio code

--- variables2.0/diccionario.py
+++ variables2.0/diccionario.py
@@ -2,6 +2,8 @@ diccionario = dict(nombre="ivan", apellido="navas",edad=20)#*creacion de un dicc
 
 print(diccionario["nombre"])
 
-diccionario2 = dict.fromkeys(["nombre","edad"])#*crea un diccionario bacio
+diccionario2 = dict.fromkeys(["nombre","edad"])#*crea un diccionario vacio
+
+diccionario2["nombre"] = "ivan"#*asi se le da el valor al diccionario
 
 print(diccionario2)
