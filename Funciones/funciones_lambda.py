756b1e7c83330170903c4e94d3b346131deeffc7
Ivan Navas <ivanrng1502@gmail.com>
2023-02-21 23:02:11 -0500

    Estudio de 21/02/2023

---
+++ Funciones/funciones_lambda.py
@@ -0,0 +1,3 @@
+saludar = lambda x: x + 1#*crea un afuncion anonima, basicamente es una funcion que no tiene nombre, paresidas a las funciones flecha de JS,(lambda retorna automaticamente)(se usa para cosas sencillas)
+
+print(saludar(2))
--- Funciones/funciones_popia.py
+++ Funciones/funciones_popia.py
@@ -8,6 +8,19 @@ def Saludo(nombre, genero):
     elif(genero == "mujer"):
         print(f"Hola {nombre}, ¿Tu eres mi alumna?")
 
+def sumar():
+    numero1 = 20
+    numero2 = 40
+    resultado = numero1 + numero2
+    return resultado#*return es lo que va a devolver una funcion, el hecho de que muestre en consola no quiere decir que esta retornando algo, ya  que el valor de la funcion será none si no retorna nada
 
-Saludo("Maria","mujer")
-Saludo("Ivan","hombre")
+def sumar_todo(nombre,*numeros):#*de esta forma tomara el valor de todos los elementos(si se quiere agregar mas parametros deben ser al inicio)
+    return f" {nombre} El resultado de tu suma es  {sum(numeros)}"#*suma todos los valores de un iterable
+
+#print(sumar_todo("Ivan, ",1,1,1,1,1,1,1,1,1,1,1,1,1,1,1))#*cuando se tiene mas parametros, se iran asignando en orden, seran la lista que tomara *numeros
+
+def prueba (nombre,edad, apellido = "Navas"):#*se puede dar valor por defecto a un parametro (si se define otra vez al momento de llamar la funcion tomara ese nuevo valor(se debe identificar el nombre del parametro, no se puede hacer solo por la posicion))
+    return f"{nombre} {apellido} {edad} "
+
+prueba2 = prueba(edad = 20 ,nombre = "Ivan", apellido = "navas")#*forzar un argumento: se espesifica el nombre del parametro y se dice a que sera igual de esta manera no importara el orden en que se definan
+print(prueba2)
